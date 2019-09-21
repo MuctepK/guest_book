704c002c807cc4acf8efc2772212f7d5013834fb
Kutman <muctep.k@gmail.com>
2019-09-21 12:31:15 +0600

    Описан класс формы, добавлена доп библиотека, описан частичный шаблон формы.

---
+++ source/webapp/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class NoteForm(forms.Form):
+    author_name = forms.CharField(max_length=70, label='Имя', required=True)
+    author_email = forms.EmailField(max_length=80, label='Email', required=True)
+    text = forms.CharField(max_length=3000, label='Текст', required=True,
+                           widget=forms.Textarea(attrs={'rows': 4, 'cols': 15}))
