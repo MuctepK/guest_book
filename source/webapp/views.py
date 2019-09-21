91d08f77beb781b08a592c6ead6fae8dcb491dcc
Kutman <muctep.k@gmail.com>
2019-09-21 12:11:22 +0600

    В приложение добавлена главная страница(index_view,index.html). Обновлена фикстура.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from webapp.models import Note, DEFAULT_STATUS
 
-# Create your views here.
+
+def index_view(request):
+    notes = Note.objects.all().filter(status=DEFAULT_STATUS).order_by('-created_at')
+    context = {'notes': notes}
+    return render(request, 'index.html', context)
+
+
+def note_create_view(request):
+    pass
+
+
+def note_delete_view(request, pk):
+    pass
+
+
+def note_update_view(request, pk):
+    pass
