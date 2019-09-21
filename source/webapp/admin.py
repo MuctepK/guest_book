47baf5303bed116687411f3c0bd6a2aa50133952
Kutman <muctep.k@gmail.com>
2019-09-21 11:49:12 +0600

    Описан класс Запись а также зарегистрирован в админке. Подключены миграции и данные забиты в фикстуру.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from webapp.models import Note
 
-# Register your models here.
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author_name', 'author_mail', 'text', 'status', 'created_at', 'updated_at']
+    list_display_links = ['author_name']
+    list_filter = ['status']
+    search_fields = ['author_name', 'text']
+    fields = ['author_name', 'author_mail', 'text', 'status']
+
+
+admin.site.register(Note, NoteAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+DEFAULT_STATUS = 'active'
+STATUS_CHOICES = [
+    ('active', 'Активно'),
+    ('blocked', 'Заблокировано')
+]
 
-# Create your models here.
+
+class Note(models.Model):
+    author_name = models.CharField(max_length=70, verbose_name='Автор записи')
+    author_mail = models.EmailField(max_length=80, verbose_name='Почта автора')
+    text = models.TextField(max_length=3000, verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    status = models.CharField(max_length=50, verbose_name='Статус', default=DEFAULT_STATUS,
+                              choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.author_name
